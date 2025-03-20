ef28de4fb2ff6c2224e4d4483dcfc4c246a24658
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-19 21:40:42 -0300

    Upload

--- repositories/messageRepository.js
+++ repositories/messageRepository.js
@@ -124,7 +124,6 @@ Por favor, digite o número da opção que você deseja:
                     clearInterval(userInterval[sender]);
                     delete userPauseStatus[sender];
                     sock.sendMessage(sender, { text: '*Pausa finalizada. A automação foi retomada.*' });
-                    sock.sendMessage(sender, { text: mainMenu });
                 }
             });
 
